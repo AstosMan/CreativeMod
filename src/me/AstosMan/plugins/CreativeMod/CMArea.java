91ff3e32b4b4595716bb5c4f67d0c970ae5952a7
h.penney <h.penney@comcast.net>
2011-10-20 00:55:09 -0500

    changed the instance vaiables to more meaningful names. Added new methods for accessing instance variables, as well as serializing and deserializing the object.

--- src/me/AstosMan/plugins/CreativeMod/CMArea.java
+++ src/me/AstosMan/plugins/CreativeMod/CMArea.java
@@ -1,31 +1,85 @@
 package me.AstosMan.plugins.CreativeMod;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import org.bukkit.Location;
-import org.bukkit.plugin.Plugin;
 
-public class CMArea implements Serializable{
+public class CMArea implements Serializable {
 	//Intended instance variables not sure that if it needs plugin
 	//but better safe then sorry.
-	private Location a, b;
-	private Plugin p;
-	private int h1, h2;
-	public CMArea(Plugin p, Location a,Location b) {
-		//Constructor defaults height to an extremely big area.
-		//TODO: Implement Serializable
-		this.p=p;
-		this.a=a;
-		this.b=b;
-		h1=0;
-		h2=1000;
+	private static final long serialVersionUID = 1L;
+	private Location l1, l2;
+	private int h0, h1;
+
+	public CMArea() {
+		//Blank Constructor
 	}
-	public CMArea(Plugin p, Location a, Location b, int h1, int h2) {
+
+	public CMArea(Location l1, Location l2) {
+		//Constructor defaults height to an extremely big area.
+		this.l1 = l1;
+		this.l2 = l2;
+		h0 = 0;
+		h1 = 1000;
+	}
+
+	public CMArea(Location l1, Location l2, int h0, int h1) {
 		//Constructor requires a height variable.
-		//TODO: Implement Serializable
-		this.p=p;
-		this.a=a;
-		this.b=b;
+		this.l1 = l1;
+		this.l2 = l2;
+		this.h0 = h0;
+		this.h1 = h1;
+	}
+
+	private void writeObject(ObjectOutputStream out) throws IOException {
+		//Object serializer
+		out.defaultWriteObject();
+	}
+
+	private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+		//Object deserializer
+		in.defaultReadObject();
+	}
+
+	public Location GetLoc1() {
+		//Returns first corner
+		return l1;
+	}
+
+	public Location GetLoc2() {
+		//Returns second corner
+		return l2;
+	}
+	
+	public int bottom() {
+		//Returns bottom level
+		return h0;
+	}
+	
+	public int top() {
+		//Returns top level
+		return h1;
+	}
+	
+	public void setBottom(int h0) {
+		//Sets Bottom level
+		this.h0=h0;
+	}
+	
+	public void setTop(int h1) {
+		//Sets top level
 		this.h1=h1;
-		this.h2=h2;
+	}
+	
+	public void setLoc1(Location l1) {
+		//Sets first corner
+		this.l1=l1;
+	}
+	
+	public void setLoc2(Location l2) {
+		//Sets second corner
+		this.l2=l2;
 	}
 }
