a7765b741d044791388c142f30c5b2422a1cb09e
h.penney <h.penney@comcast.net>
2011-10-22 15:30:02 -0500

    Added boolean to CMArea for tnt state in the area. It defaults to true. Added methods to CMArea to get and change tnt state in the area.
    Added an arraylist for CMareas in the EntityListener with an add and remove method for th CMAreas.
    Overrided onExplosionPrimeEvent so that if its tntprimeevent and within a creative area that has set tnt state to off it will cancel the explosion.

--- src/me/AstosMan/plugins/CreativeMod/CMEntityListener.java
+++ src/me/AstosMan/plugins/CreativeMod/CMEntityListener.java
@@ -1,14 +1,40 @@
 package me.AstosMan.plugins.CreativeMod;
 
+import java.util.ArrayList;
+
+import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.entity.EntityListener;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.bukkit.plugin.Plugin;
 
 public class CMEntityListener extends EntityListener
 {
 	private Plugin plugin;
+	private ArrayList<CMArea> CMAreas;
 	
 	public CMEntityListener(Plugin p)
 	{
 		this.plugin = p;
 	}
+	
+	public void addArea(CMArea CMArea) {
+		CMAreas.add(CMArea);
+	}
+	
+	public void removeArea(CMArea CMArea) {
+		CMAreas.remove(CMArea);
+	}
+	
+	public void onExplosionPrimeEvent(ExplosionPrimeEvent event){
+		// If TNT is primed and within a creative area that has set TNT to off this method will cancel the explosion.
+		if (event.getEntity() instanceof TNTPrimed) {
+			for (int i = 0; i < CMAreas.size(); i++) {
+				if (CMAreas.get(i).Contains(event.getEntity().getLocation()) && CMAreas.get(i).getTrinitrotolueneState()) {
+					event.setCancelled(true);
+					return;
+				}
+			}
+		}
+	}
+	
 }
