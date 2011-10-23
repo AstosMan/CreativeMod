426daa19ac1bfeada63ccd6fbb23905211013dca
Anthony Phelps <anthonypphelps@gmail.com>
2011-10-22 20:45:43 -0500

    Started some work on CMPlayerListener

--- src/me/AstosMan/plugins/CreativeMod/CMPlayerListener.java
+++ src/me/AstosMan/plugins/CreativeMod/CMPlayerListener.java
@@ -1,6 +1,8 @@
 package me.AstosMan.plugins.CreativeMod;
 
+import org.bukkit.Location;
 import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.Plugin;
 
 public class CMPlayerListener extends PlayerListener 
@@ -10,5 +12,18 @@ public class CMPlayerListener extends PlayerListener
 	public CMPlayerListener(Plugin p)
 	{
 		this.plugin = p;
+	}
+	
+	public void onPlayerMove(PlayerMoveEvent pme)
+	{
+		Location l = pme.getPlayer().getLocation();
+		double x = l.getX(), y = l.getY(), z = l.getZ();
+		for(CMArea cma : ((CreativeMod)plugin).getAreas())
+		{
+			if()
+			{
+				
+			}
+		}
 	}
 }
