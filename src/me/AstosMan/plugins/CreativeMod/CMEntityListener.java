70ba660e3e72816024d54b24fd16af188467d63f
h.penney <h.penney@comcast.net>
2011-10-23 21:08:41 -0500

    Didn't know that that was possible for arrays now I do and have changed it.
    Also implemented a better version of tnt cancellation that includes creepers hopefully (I don't know for sure).

--- src/me/AstosMan/plugins/CreativeMod/CMEntityListener.java
+++ src/me/AstosMan/plugins/CreativeMod/CMEntityListener.java
@@ -1,8 +1,5 @@
 package me.AstosMan.plugins.CreativeMod;
 
-import java.util.ArrayList;
-
-import org.bukkit.entity.CreatureType;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Monster;
 import org.bukkit.entity.TNTPrimed;
@@ -14,28 +11,17 @@ import org.bukkit.plugin.Plugin;
 public class CMEntityListener extends EntityListener
 {
 	private Plugin plugin;
-	private ArrayList<CMArea> CMAreas;
 	
 	public CMEntityListener(Plugin p)
 	{
 		this.plugin = p;
 	}
 	
-	public void addArea(CMArea CMArea) {
-		// Adds a CMArea to CMAreas
-		CMAreas.add(CMArea);
-	}
-	
-	public void removeArea(CMArea CMArea) {
-		// Removes a CMArea from CMAreas
-		CMAreas.remove(CMArea);
-	}
-	
 	public void onCreatureSpawn(CreatureSpawnEvent event) {
 		// If Mob is spawned and within a creative area that has set Mobs to off this method will cancel the spawn.
 		if (event.getEntity() instanceof Monster) {
-			for(CMArea area : CMAreas) {
-				if (area.Contains(event.getEntity().getLocation()) && area.getMobEntranceState()) {
+			for(CMArea cma : ((CreativeMod)plugin).getAreas()) {
+				if (cma.Contains(event.getEntity().getLocation()) && cma.getMobEntranceState()) {
 					event.setCancelled(true);
 					return;
 				}
@@ -49,8 +35,8 @@ public class CMEntityListener extends EntityListener
 	public void onExplosionPrimeEvent(ExplosionPrimeEvent event){
 		// If TNT is primed and within a creative area that has set TNT to off this method will cancel the explosion.
 		if (event.getEntity() instanceof TNTPrimed || event.getEntity() instanceof Creeper) {
-			for (CMArea area : CMAreas) {
-				if (area.Contains(event.getEntity().getLocation()) && area.getTrinitrotolueneState()) {
+			for (CMArea cma : ((CreativeMod)plugin).getAreas()) {
+				if (cma.Contains(event.getEntity().getLocation()) && cma.getTrinitrotolueneState()) {
 					event.setCancelled(true);
 					return;
 				}
